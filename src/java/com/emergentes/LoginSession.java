c61e9e4d1a986756f076694718196c008f5ee361
Jorge <Jorge@DESKTOP-EI54MJL>
2021-03-23 21:46:19 -0400

    primerComit

---
+++ src/java/com/emergentes/LoginSession.java
@@ -0,0 +1,55 @@
+
+package com.emergentes;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ *
+ * @author Jorge
+ */
+@WebServlet(name = "LoginSession", urlPatterns = {"/LoginSession"})
+public class LoginSession extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        HttpSession ses = request.getSession();
+        String login= (String) ses.getAttribute("login");
+        if(login.equals("OK")){
+            //termina la sesion
+            ses.invalidate();
+            response.sendRedirect("index.jsp");
+        }
+       
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        HttpSession ses = request.getSession();
+        String usuario = request.getParameter("usuario");
+        String password = request.getParameter("password");
+        
+        if(usuario.equals("admin") && (password.equals("1234"))){
+            ses.setAttribute("login", "OK");
+            ses.setAttribute("usuario", usuario);
+            //deseamosingresar
+            response.sendRedirect("panel.jsp");
+        }
+        else{
+            ses.setAttribute("error", "Usuario sin autorizacion" );
+            response.sendRedirect("login.jsp");
+        }
+        
+        
+    }
+    
+
+}
---
+++ web/index.jsp
@@ -0,0 +1,24 @@
+<%-- 
+    Document   : index
+    Created on : 23-mar-2021, 21:11:46
+    Author     : Jorge
+--%>
+
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>JSP Page</title>
+    </head>
+    <body>
+        <h1>Bienvenido al Sitio Web</h1>
+        <a href="login.jsp">Login</a>
+        <p>
+           Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quisquam quam 
+           quae possimus accusantium reprehenderit, soluta quas magnam modi, quos in 
+           repellendus molestias perferendis consequuntur. Iure autem error beatae i
+           mpedit maiores.
+        </p>
+    </body>
+</html>
